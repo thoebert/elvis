0dc65d16b627a04a61c2e479483919c638bebba1
Timon Höbert <timon.hoebert@gmx.at>
2019-05-13 11:52:48 +0200

    Updates

---
+++ precache-manifest.76b2be555b391c6afe2cb93c1b5983c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9300a612191e0ca9869024e89a589b35",
+    "url": "/elvis/index.html"
+  },
+  {
+    "revision": "98cbedcda50d00d9931f",
+    "url": "/elvis/static/css/2.600dfb7d.chunk.css"
+  },
+  {
+    "revision": "760be72aac81a5519ca0",
+    "url": "/elvis/static/css/main.d484ed85.chunk.css"
+  },
+  {
+    "revision": "98cbedcda50d00d9931f",
+    "url": "/elvis/static/js/2.ff97481a.chunk.js"
+  },
+  {
+    "revision": "760be72aac81a5519ca0",
+    "url": "/elvis/static/js/main.45c80350.chunk.js"
+  },
+  {
+    "revision": "ee9047221a48cd62a7cc",
+    "url": "/elvis/static/js/runtime~main.a0bc3905.js"
+  }
+]);
